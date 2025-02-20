a8bebb1abba2f3a2e322e6aaac5889076fe635a5
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-02-20 11:58:57 -0800

    idk what going on man

--- backend/src/main/java/com/stockanalysis/config/AWSConfig.java
+++ backend/src/main/java/com/stockanalysis/config/AWSConfig.java
@@ -6,7 +6,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.comprehend.ComprehendClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
@@ -35,12 +34,6 @@ public class AWSConfig {
                 .build();
     }
 
-    public ComprehendClient buildComprehendClient() {
-        return ComprehendClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
-                .build();
-    }
 
     public String getBucketName() {
         return bucketName;
