1ca7e190242347469492455055cffc8f9136ec2b
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-02-20 11:47:04 -0800

    Revert "split chrome driver into its own configuration for implementation in Selenium Chrome Driver tests"
    This reverts commit 23e869e1566d8c2be7e3a2f6f400e7401fb863c2.

--- backend/src/main/java/com/stockanalysis/config/AWSConfig.java
+++ backend/src/main/java/com/stockanalysis/config/AWSConfig.java
@@ -6,6 +6,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.comprehend.ComprehendClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
@@ -34,6 +35,12 @@ public class AWSConfig {
                 .build();
     }
 
+    public ComprehendClient buildComprehendClient() {
+        return ComprehendClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
 
     public String getBucketName() {
         return bucketName;
